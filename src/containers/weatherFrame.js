48fd68b0259fd27d2aa7c1a57965e08b636db457
Daniel Brazel <dbrazel4@gmail.com>
2018-10-01 10:30:45 -0400

    fixed fetch started mapping weather to DOM

--- src/containers/weatherFrame.js
+++ src/containers/weatherFrame.js
@@ -4,18 +4,36 @@ import { key } from '../apiKey.js';
 import '../weather.css';
 
 class WeatherFrame extends Component {
-  locationGet = () => {
-    fetch(`api.openweathermap.org/data/2.5/weather?q=brooklyn&APPID=${key}`, {
-      cache: 'default'
-    })
-    .then(res => res.json())
-    .then(json => console.log(json))
+  constructor(){
+    super()
+    this.state = {
+      weather: {}
+    }
   }
+
+  statusCheck = (response) => {
+              if (response.status === 200) {
+                console.log('Success!!');
+              } else {
+                console.log('Failed');
+              }
+            }
+
+  locationGet = () => {
+    fetch("http://api.openweathermap.org/data/2.5/weather?id=5128638&APPID=8d2e0f9140b10ca988b3cff5f9dfd658")
+	   .then(res => res.json())
+	   .then(json => {
+		     this.setState = {weather: json.weather[0]}
+	      })
+      }
 
   render(){
     return(
       <React.Fragment>
         <button onClick={this.locationGet}>Get Weather</button>
+        <p>{this.state.weather.map(el => {
+            return <p>{el}</p>
+          })}</p>
       </React.Fragment>
     )
   }
